0659bd3bb54d7ebdeded881459c226340faa8e62
magarteem <fmf311@mail.ru>
2022-11-16 13:49:43 +0200

    react-use-pwa-install

--- src/modules/pwa/ButtonInstallPwa.tsx
+++ src/modules/pwa/ButtonInstallPwa.tsx
@@ -1,43 +1,51 @@
-import React, { useEffect, useState } from "react";
+import { usePWAInstall } from "react-use-pwa-install";
 import s from "./pwa.module.scss";
 
 export const ButtonInstallPwa = () => {
- const [supportsPWA, setSupportsPWA] = useState(false);
- const [promptInstall, setPromptInstall] = useState(null);
+ const install = usePWAInstall();
+ // const [supportsPWA, setSupportsPWA] = useState(false);
+ // const [promptInstall, setPromptInstall] = useState(null);
 
- useEffect(() => {
-  const handler = (e: any) => {
-   e.preventDefault();
-   console.log("we are being triggered :D");
-   setSupportsPWA(true);
-   setPromptInstall(e);
-  };
-  window.addEventListener("beforeinstallprompt", handler);
+ // useEffect(() => {
+ //  const handler = (e: any) => {
+ //   e.preventDefault();
+ //   console.log("we are being triggered :D");
+ //   setSupportsPWA(true);
+ //   setPromptInstall(e);
+ //  };
+ //  window.addEventListener("beforeinstallprompt", handler);
 
-  return () =>
-   window.removeEventListener("transitionend", handler);
- }, []);
+ //  return () =>
+ //   window.removeEventListener("transitionend", handler);
+ // }, []);
 
- const onClick = (evt: any) => {
-  evt.preventDefault();
-  if (!promptInstall) {
-   return;
-  }
-  //@ts-ignore
-  promptInstall.prompt();
- };
- if (!supportsPWA) {
-  return null;
- }
+ // const onClick = (evt: any) => {
+ //  evt.preventDefault();
+ //  if (!promptInstall) {
+ //   return;
+ //  }
+ //  //@ts-ignore
+ //  promptInstall.prompt();
+ // };
+ // if (!supportsPWA) {
+ //  return null;
+ // }
  return (
-  <button
-   className={s.pwa}
-   id="setup_button"
-   aria-label="Install app"
-   title="Install app"
-   onClick={onClick}
-  >
-   Установить как приложение
-  </button>
+  <>
+   {install && (
+    <button className={s.pwa} onClick={install}>
+     Установить как приложение
+    </button>
+   )}
+  </>
+  //<button
+  // className={s.pwa}
+  // id="setup_button"
+  // aria-label="Install app"
+  // title="Install app"
+  // onClick={onClick}
+  //>
+  // Установить как приложение
+  //</button>
  );
 };
