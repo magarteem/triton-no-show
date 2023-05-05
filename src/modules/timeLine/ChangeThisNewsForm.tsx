6df89950302373e1e0047e992b4d771a334d0626
magarteem <fmf311@mail.ru>
2023-05-06 02:39:48 +0300

    ts-2

--- src/modules/timeLine/ChangeThisNewsForm.tsx
+++ src/modules/timeLine/ChangeThisNewsForm.tsx
@@ -9,93 +9,93 @@ import clearIcon from "../../assets/icons/clearIcon.svg";
 import { ButtonSubmitMui } from "../../common/mui-element/ButtonSubmitMui";
 import { CustomButtomAddImg } from "../../common/components/timeLine/customButtomAddImg/CustomButtomAddImg";
 import { CityResultsType } from "../../types/PROFILE/cityGlobalType";
-import { SelectTypeNews } from "./formFieldsSpecificStyle/SelectTypeNews";
+import { SelectTypeNews } from "../../common/mui-element/typeNews/SelectTypeNews";
 
 interface ChangeNewNewsFormType {
-  changeDataNews: any;
+ changeDataNews: any;
 }
 
 export const ChangeThisNewsForm = ({ changeDataNews }: ChangeNewNewsFormType) => {
-  const navigate = useNavigate();
+ const navigate = useNavigate();
 
-  const { timeLinePost, author, date, id } = changeDataNews;
+ const { timeLinePost, author, date, id } = changeDataNews;
 
-  const {
-    watch,
-    control,
-    register,
-    handleSubmit,
-    setValue,
-    formState: { errors, isValid },
-  } = useForm<any>({
-    mode: "onBlur",
-    defaultValues: {
-      genre: timeLinePost.genre,
-      tools: timeLinePost.tools || [],
-      photo: timeLinePost.photo,
-      text: timeLinePost.text,
-      typeCategory: timeLinePost.typeCategory,
-      city: timeLinePost.city || {},
-    },
-  });
+ const {
+  watch,
+  control,
+  register,
+  handleSubmit,
+  setValue,
+  formState: { errors, isValid },
+ } = useForm<any>({
+  mode: "onBlur",
+  defaultValues: {
+   genre: timeLinePost.genre,
+   tools: timeLinePost.tools || [],
+   photo: timeLinePost.photo,
+   text: timeLinePost.text,
+   typeCategory: timeLinePost.typeCategory,
+   city: timeLinePost.city || {},
+  },
+ });
 
-  const onSubmit = (data: any) => {
-    //myProfile &&
-    //  dispatch(
-    //    updateDataTimeLineThunk({
-    //      date,
-    //      id,
-    //      timeLinePost: {
-    //        ...data,
-    //        genre: data.genre,
-    //        tools: data.tools,
-    //        photo: data.photo,
-    //        text: data.text,
-    //        typeCategory: data.typeCategory,
-    //        city: data.city,
-    //      },
-    //      author,
-    //    })
-    //  );
-    navigate(-1);
-  };
+ const onSubmit = (data: any) => {
+  //myProfile &&
+  //  dispatch(
+  //    updateDataTimeLineThunk({
+  //      date,
+  //      id,
+  //      timeLinePost: {
+  //        ...data,
+  //        genre: data.genre,
+  //        tools: data.tools,
+  //        photo: data.photo,
+  //        text: data.text,
+  //        typeCategory: data.typeCategory,
+  //        city: data.city,
+  //      },
+  //      author,
+  //    })
+  //  );
+  navigate(-1);
+ };
 
-  const clearPhoto = () => {
-    setValue("photo", [""]);
-  };
+ const clearPhoto = () => {
+  setValue("photo", [""]);
+ };
 
-  return (
-    <form noValidate onSubmit={handleSubmit(onSubmit)} className={s.formWrapCreateNews}>
-      <section className={s.textFields}>
-        <div className={s.styleInput}>
-          <Controller
-            name="text"
-            control={control}
-            render={({ field: { onChange, value, ref, ...field } }) => (
-              <div className={s.sizeInput}>
-                <TextField
-                  value={value}
-                  multiline
-                  sx={styleTextAreaSX}
-                  fullWidth
-                  autoComplete="off"
-                  placeholder="Чем вы хотите поделиться?"
-                  variant="outlined"
-                  onChange={onChange}
-                />
-              </div>
-            )}
-          />
-        </div>
+ return (
+  <form noValidate onSubmit={handleSubmit(onSubmit)} className={s.formWrapCreateNews}>
+   <section className={s.textFields}>
+    <div className={s.styleInput}>
+     <Controller
+      name="text"
+      control={control}
+      render={({ field: { onChange, value, ref, ...field } }) => (
+       <div className={s.sizeInput}>
+        <TextField
+         value={value}
+         multiline
+         sx={styleTextAreaSX}
+         fullWidth
+         autoComplete="off"
+         placeholder="Чем вы хотите поделиться?"
+         variant="outlined"
+         onChange={onChange}
+        />
+       </div>
+      )}
+     />
+    </div>
 
-        <div className={s.imgWrap}>
-          {/*<img src={cardsItem_1} alt={cardsItem_1} /> // img удалена*/}
-          <img onClick={clearPhoto} className={s.clearImgButton} src={clearIcon} alt="clear" />
-        </div>
-      </section>
+    <div className={s.imgWrap}>
+     {/*<img src={cardsItem_1} alt={cardsItem_1} /> // img удалена*/}
+     <img onClick={clearPhoto} className={s.clearImgButton} src={clearIcon} alt="clear" />
+    </div>
+   </section>
 
-      <section className={s.selectParams}>
-        {/*<div className={s.selectFieldCustomHeight}>
+   <section className={s.selectParams}>
+    {/*<div className={s.selectFieldCustomHeight}>
      <Controller
       name="genre"
       control={control}
@@ -113,7 +113,7 @@ export const ChangeThisNewsForm = ({ changeDataNews }: ChangeNewNewsFormType) =>
      />
     </div>*/}
 
-        {/*<div className={cn(s.selectFieldCustomHeight, s.addMargin)}>
+    {/*<div className={cn(s.selectFieldCustomHeight, s.addMargin)}>
           <Controller
             name="tools"
             control={control}
@@ -131,85 +131,85 @@ export const ChangeThisNewsForm = ({ changeDataNews }: ChangeNewNewsFormType) =>
           />
         </div>*/}
 
-        <div className={s.selectBlock}>
-          <div className={s.selectField}>
-            <SelectTypeNews
-              required={true}
-              control={control}
-              placeholder="Тип новости"
-              options={optionСategoryBD}
-              name="typeCategory"
-            />
-          </div>
+    <div className={s.selectBlock}>
+     <div className={s.selectField}>
+      <SelectTypeNews
+       required={true}
+       control={control}
+       placeholder="Тип новости"
+       options={optionСategoryBD}
+       name="typeCategory"
+      />
+     </div>
 
-          <div className={s.selectField}>
-            <SelectTypeNews
-              control={control}
-              placeholder="Город"
-              //@ts-ignore
-              options={data?.results?.map((x: CityResultsType) => {
-                return {
-                  id: x.id,
-                  name: x.title,
-                };
-              })}
-              name="city"
-            />
-          </div>
-        </div>
+     <div className={s.selectField}>
+      <SelectTypeNews
+       control={control}
+       placeholder="Город"
+       //@ts-ignore
+       options={data?.results?.map((x: CityResultsType) => {
+        return {
+         id: x.id,
+         name: x.title,
+        };
+       })}
+       name="city"
+      />
+     </div>
+    </div>
 
-        <div className={s.sendBlock}>
-          <div className={cn(s.btnWrapper, s.imgSend)}>
-            <Controller
-              name="tools"
-              control={control}
-              render={({ field: { onChange, value, ref, ...field }, formState: { errors } }) => (
-                <CustomButtomAddImg
-                  startIcon={<AddImageIcons />}
-                  textButton="Добавить"
-                  isValidInButton={!Array.isArray(watch("photo"))}
-                  register={register}
-                  onChange={onChange}
-                />
-              )}
-            />
-          </div>
-          <div className={s.btnWrapper}>
-            <ButtonSubmitMui textButton="Сохранить изменения" isValidInButton={!isValid} />
-          </div>
-        </div>
-      </section>
-    </form>
-  );
+    <div className={s.sendBlock}>
+     <div className={cn(s.btnWrapper, s.imgSend)}>
+      <Controller
+       name="tools"
+       control={control}
+       render={({ field: { onChange, value, ref, ...field }, formState: { errors } }) => (
+        <CustomButtomAddImg
+         startIcon={<AddImageIcons />}
+         textButton="Добавить"
+         isValidInButton={!Array.isArray(watch("photo"))}
+         register={register}
+         onChange={onChange}
+        />
+       )}
+      />
+     </div>
+     <div className={s.btnWrapper}>
+      <ButtonSubmitMui textButton="Сохранить изменения" isValidInButton={!isValid} />
+     </div>
+    </div>
+   </section>
+  </form>
+ );
 };
 
 export const styleTextAreaSX = {
-  width: "100% !important",
-  fontSize: "16px",
+ width: "100% !important",
+ fontSize: "16px",
+ fontFamily: `Mulish_Regular, sans-serif !important`,
+ padding: "0",
+
+ fieldset: {
+  display: "none",
+ },
+
+ "& .MuiInputBase-root": {
+  height: "auto",
+  borderRadius: "8px",
+  padding: "0 14px",
+  color: "#43483E",
   fontFamily: `Mulish_Regular, sans-serif !important`,
-  padding: "0",
 
-  fieldset: {
-    display: "none",
+  "&:focus": {
+   height: "auto",
   },
 
-  "& .MuiInputBase-root": {
-    height: "auto",
-    borderRadius: "8px",
-    padding: "0 14px",
-    color: "#43483E",
-    fontFamily: `Mulish_Regular, sans-serif !important`,
-
-    "&:focus": {
-      height: "auto",
-    },
-
-    "& .MuiInputBase-input": {
-      "&::-webkit-input-placeholder": {
-        fontWeight: 500,
-        color: "#242424",
-        opacity: 1,
-      },
-    },
+  "& .MuiInputBase-input": {
+   "&::-webkit-input-placeholder": {
+    fontWeight: 500,
+    color: "#242424",
+    opacity: 1,
+   },
   },
+ },
 };
